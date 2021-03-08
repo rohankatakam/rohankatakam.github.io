9556d455d79ed2e059d036ed095e3fc98db8b906
Rohan Katakam <katakam2@illinois.edu>
2021-03-08 02:50:07 -0600

    Updates

---
+++ precache-manifest.065bfb171fdf4bfe47372f98881bbf81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868ddea83f24693c497232a9435d7d50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70741e9f81ed04a17a41",
+    "url": "/static/css/main.4e0c9150.chunk.css"
+  },
+  {
+    "revision": "abae4320d24d31b3db16",
+    "url": "/static/js/2.456a4cca.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/static/js/2.456a4cca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70741e9f81ed04a17a41",
+    "url": "/static/js/main.fb5f35a3.chunk.js"
+  },
+  {
+    "revision": "c0d7c58e510620f2f64c",
+    "url": "/static/js/runtime-main.7b61e083.js"
+  },
+  {
+    "revision": "2dfdb3ecb352b5746e5ad1fac34e9118",
+    "url": "/static/media/portcrop.2dfdb3ec.jpg"
+  }
+]);
